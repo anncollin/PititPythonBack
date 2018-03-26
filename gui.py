e4fd4c5da139936e75836136a89f5e93cb2e6506
vanderschuea <antoine.vanderschueren@student.uclouvain.be>
2018-03-26 12:20:29 +0200

    Added main for final report

--- gui.py
+++ gui.py
@@ -1,9 +1,13 @@
 from pygame import Surface, image, draw, font
 from board import Board
+from collections import defaultdict
+from autoplay import *
+from MDP import *
 import numpy as np
+import os
 
 
-def draw_board(board, dice, expec, ref_expec, filename):
+def draw_board(board, dice, expec, ref_expec, directory, filename):
     # Color initialization
     red = (255, 0, 0)
     green = (0, 255, 0)
@@ -83,7 +87,6 @@ def draw_board(board, dice, expec, ref_expec, filename):
         # Outer border
         draw.rect(surf, black, (x, y, size, size), 3)
 
-
     font.init()
     for x, y, ind in zip(x_co, y_co, co_map):
         if ind != 14:
@@ -91,7 +94,6 @@ def draw_board(board, dice, expec, ref_expec, filename):
         else:
             draw.rect(surf, black, (x, y, size, size), 0)
             draw.rect(surf, black, (x, y, size, size), 3)
-
 
     # draw lines
     x_l = surfM + size + np.append(np.append(1.5 * size * np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
@@ -106,12 +108,37 @@ def draw_board(board, dice, expec, ref_expec, filename):
         draw.line(surf, black, (x, y), (x + d[0] * size/2, y + d[1] * size * 0.5), 3)
 
     # save picture
-    image.save(surf, filename)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    image.save(surf, os.path.join(directory, filename))
 
 
 if __name__ == "__main__":
-    board = [x.type for x in Board({'trap1': [1, 10], 'trap2': [3, 5, 7]}).board]
-    dice = [2, 1, 3, 3, 2, 3, 3, 3, 2, 1, 1, 3, 2, 1, 1]
-    expec = [11.698915143254899, 11.032249954001127, 9.032252820017574, 9.701159919483432, 8.43010450515775, 7.401431295786926, 5.643368090782532, 4.232526126289438, 2.5, 2.0, 4.833333333333332, 2.833333333333333, 2.5, 2.0, 0]
-    ref_expec = expec[0]
-    draw_board(board, dice, expec, ref_expec, "board.png")
+    seed(456)
+    boards = [
+        Board(defaultdict(list), circling=False), Board(defaultdict(list), circling=False),
+              Board({"trap1": [10], "trap2": [4, 5, 8], "trap3":[]}, circling=False),
+              Board({"trap1": [9, 13], "trap2": [8, 12, 4], "trap3": [1, 10, 11]}, circling=True)
+              ]
+    names = ["p1", "p2", "p3", "p4"]
+    for board, name in zip(boards, names):
+        mdp_expec, mdp_dice = markovDecision(board)
+        # mdp_dice = find_strategy(board, n_games=5000, very_intelligent=True)
+        mdp_expec = play_strategy(board, lambda x: mdp_dice[x]-1, n_games=10000)
+
+        ag_dice = find_strategy(board, n_games=1000, very_intelligent=False)
+        ag_expec = play_strategy(board, lambda x: ag_dice[x], n_games=10000)
+
+        rand_expec = play_strategy(board, lambda x: randrange(3), n_games=10000)
+
+        draw_board([x.type for x in board.board], mdp_dice, mdp_expec, ag_expec[0], name,"mdp"+".png")
+        draw_board([x.type for x in board.board], [d+1 for d in ag_dice], ag_expec, ag_expec[0], name, "ag" + ".png")
+        draw_board([x.type for x in board.board], [0]*15, rand_expec, ag_expec[0], name, "rand" + ".png")
+
+    # ' & '.join([str(x) for x in a])+'\\'
+
+    # board = [x.type for x in Board({'trap1': [1, 10], 'trap2': [3, 5, 7]}).board]
+    # dice = [2, 1, 3, 3, 2, 3, 3, 3, 2, 1, 1, 3, 2, 1, 1]
+    # expec = [11.698915143254899, 11.032249954001127, 9.032252820017574, 9.701159919483432, 8.43010450515775, 7.401431295786926, 5.643368090782532, 4.232526126289438, 2.5, 2.0, 4.833333333333332, 2.833333333333333, 2.5, 2.0, 0]
+    # ref_expec = expec[0]
+    # draw_board(board, dice, expec, ref_expec, "board.png")
